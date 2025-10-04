761a096bd544f05f2f9304bc40707b220e78daf4
Jorge Nuricumbo <Jorge.Nuricumbo@nuxos.io>
2025-10-04 18:40:02 +0200

    Refactor main logic and extract document loading to a new `loader.py` module.

---
+++ loader.py
@@ -0,0 +1,26 @@
+﻿from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_docling import DoclingLoader
+from langchain_milvus import Milvus
+
+load_dotenv()
+
+llm = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+vectorstore = Milvus(
+    embedding_function=embeddings,
+    collection_name="clinicalGuides",
+    connection_args={"uri": "http://localhost:19530", "token": "root:Milvus", "db_name": "enarmy"},
+    index_params={"index_type": "FLAT", "metric_type": "L2"},
+    consistency_level="Strong",
+    drop_old=False,  # set to True if seeking to drop the collection with that name if it exists
+)
+
+# Load and chunk content
+FILE_PATH = "./data/081GER_1.pdf"
+loader = DoclingLoader(file_path=FILE_PATH)
+docs = loader.load()
+
+# Index chunks
+_ = vectorstore.add_documents(documents=docs)
--- query.py
+++ query.py
@@ -1,32 +1,21 @@
-from dotenv import load_dotenv
-from langchain.chat_models import init_chat_model
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+﻿from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain import hub
 from langchain_core.documents import Document
 from langgraph.graph import START, StateGraph
 from typing_extensions import List, TypedDict
-from langchain_docling import DoclingLoader
 from langchain_milvus import Milvus
-
-load_dotenv()
+from langchain.chat_models import init_chat_model
 
 llm = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
 embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
 vectorstore = Milvus(
     embedding_function=embeddings,
+    collection_name="clinicalGuides",
     connection_args={"uri": "http://localhost:19530", "token": "root:Milvus", "db_name": "enarmy"},
     index_params={"index_type": "FLAT", "metric_type": "L2"},
     consistency_level="Strong",
     drop_old=False,  # set to True if seeking to drop the collection with that name if it exists
 )
-
-# Load and chunk contents of the blog
-FILE_PATH = "./data/081GER_1.pdf"
-loader = DoclingLoader(file_path=FILE_PATH)
-docs = loader.load()
-
-# Index chunks
-_ = vectorstore.add_documents(documents=docs)
 
 # Define prompt for question-answering
 # N.B. for non-US LangSmith endpoints, you may need to specify
@@ -39,12 +28,10 @@ class State(TypedDict):
     context: List[Document]
     answer: str
 
-
 # Define application steps
 def retrieve(state: State):
     retrieved_docs = vectorstore.similarity_search(state["question"])
     return {"context": retrieved_docs}
-
 
 def generate(state: State):
     docs_content = "\n\n".join(doc.page_content for doc in state["context"])
@@ -52,12 +39,10 @@ def generate(state: State):
     response = llm.invoke(messages)
     return {"answer": response.content}
 
-
 # Compile application and test
 graph_builder = StateGraph(State).add_sequence([retrieve, generate])
 graph_builder.add_edge(START, "retrieve")
 graph = graph_builder.compile()
 
-response = graph.invoke({"question": "que es la leucorrea?"})
+response = graph.invoke({"question": "que hacer si hay Vaginitis por Candida sp y existe embarazo?"})
 print(response["answer"])
-
